79dc53a42f04de2705a6de4bd0444851d80c49ba
J.P. Hamilton <instantjp@gmail.com>
2017-06-01 17:06:10 -0500

    fixed util/isFunction bug (#373)
    * fixed util/isFunction bug
    
    * removed isFunction

--- src/jquery.floatThead.js
+++ src/jquery.floatThead.js
@@ -49,7 +49,7 @@
     }
   };
 
-  var util = window._ || (function underscoreShim(){
+  var util = (function underscoreShim(){
     var that = {};
     var hasOwnProperty = Object.prototype.hasOwnProperty, isThings = ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp'];
     that.has = function(obj, key) {
